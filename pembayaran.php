e44f61bde800c11b248cf56b061a5a6a40c17afc
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2022-11-22 20:50:25 +0700

    Added Midtrans support

--- pembayaran.php
+++ pembayaran.php
@@ -1,5 +1,32 @@
 <?php
+    require_once dirname(__FILE__) . '/Midtrans.php';
 
+    // Set your Merchant Server Key
+    \Midtrans\Config::$serverKey = 'SB-Mid-server-_VkLv7E_ab9hH4p3yOnyQ73l';
+    // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+    \Midtrans\Config::$isProduction = false;
+    // Set sanitization on (default)
+    \Midtrans\Config::$isSanitized = true;
+    // Set 3DS transaction for credit card to true
+    \Midtrans\Config::$is3ds = true;
+    
+    $params = array(
+        'transaction_details' => array(
+            'order_id' => rand(),
+            'gross_amount' => 10000,
+        ),
+        'customer_details' => array(
+            'first_name' => 'Kurasa',
+            'last_name' => 'Takada',
+            'email' => 'YONDAKTAUKOKTANYASAYA@example.com',
+            'phone' => '08111222333',
+        ),
+    );
+    
+    $snapToken = \Midtrans\Snap::getSnapToken($params);
+    $snapURL = \Midtrans\Snap::getSnapUrl($params);
+    header("Location: ".$snapURL);
+    echo $snapToken;
 ?>
 <!DOCTYPE html>
 <html lang="en">

d4be0c14f9681539086310e87df85efb53ff29e1
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2022-12-03 10:34:01 +0700

    update

--- detail.php
+++ detail.php
@@ -150,13 +150,39 @@ if(isset($_POST["back"])){
                 }
               ?>
             </div>
-            <button type="submit" name="keranjang" class="btn btn-primary">Tambahkan ke Keranjang</button>
+            <button type="submit" name="keranjang" data-toggle="modal" class="btn btn-primary">Tambahkan ke Keranjang</button>
             <button type="submit" name="back" class="btn btn-primary">Back</button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    ...
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </form>
         </div>
       </div>
     <!-- Konten -->
-
+    <script>
+      $('#exampleModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+      })
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>

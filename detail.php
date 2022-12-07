61776feb70b11c11ddfad383b503bb17a9180089
jessicas16 <101440484+jessicas16@users.noreply.github.com>
2022-12-07 22:19:55 +0700

    bru dikit

--- detail.php
+++ detail.php
@@ -92,7 +92,7 @@ if(isset($_POST["back"])){
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title">Modal title</h5>
+            <h5 class="modal-title">Confirmation</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">

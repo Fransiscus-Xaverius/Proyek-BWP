cb93ffc57ac127ccb8a1d11613b63f9538cbf8e0
jessicas16 <101440484+jessicas16@users.noreply.github.com>
2022-12-08 21:28:13 +0700

    Update fetchTrans.php

--- admin/fetchTrans.php
+++ admin/fetchTrans.php
@@ -2,13 +2,14 @@
 
     require("helper.php");
 
-    $allTrans = mysqli_query($con, "SELECT * from htrans");
+    $allTrans = mysqli_query($con, "SELECT * from htrans h, customer c where h.id_customer = c.id_customer");
 
     echo "<table border=1>";
     echo "<tr height = '40px'>";
     echo "<th>No</th>";
     echo "<th>ID Transaksi</th>";
     echo "<th>ID Customer</th>";
+    echo "<th>Nama Customer</th>";
     echo "<th>Harga Total</th>";
     echo "<th>Tanggal Transaksi</th>";
     echo "<th>Order ID</th>";
@@ -20,7 +21,8 @@
         echo "<td>" . $no . "</td>";
         echo "<td>" . $row['htrans_id'] . "</td>";
         echo "<td>" . $row['id_customer'] . "</td>";
-        echo "<td>" . $row['harga_total'] . "</td>";
+        echo "<td>" . $row['nama_customer'] . "</td>";
+        echo "<td> Rp. " . number_format($row['harga_total'], 0, ',', '.') . "</td>";        
         echo "<td>" . $row['h_date'] . "</td>";
         echo "<td>" . $row['order_id'] . "</td>";
         echo "<td>";     
